2d3ed3660fbe7d4cdf97c8ac0ab43e7b572c6600
Lukas Lipp <lippls@hotmail.com>
2022-09-28 17:37:51 +0200

    some fixes

--- make.bat
+++ make.bat
@@ -1,26 +1,23 @@
 @ECHO OFF
 if "%~1"=="" goto BLANK
-if "%~1"=="22" goto 22
 if "%~1"=="install" goto install
 if "%~1"=="clean" goto CLEAN
 @ECHO ON
 
 :BLANK
-cmake -H. -B_project -G  "Visual Studio 16 2019" -A "x64"
-GOTO DONE
-
-:22
-cmake -H. -B_project -G  "Visual Studio 17 2022" -A "x64"
+cmake -H. -B_project -A "x64" -DCMAKE_INSTALL_PREFIX="_install"
 GOTO DONE
 
 :INSTALL
-cmake -H. -B_project -G  "Visual Studio 16 2019" -A "x64" -DCMAKE_INSTALL_PREFIX=%2 
+set @location="_install"
+if NOT "%2"=="" set @location="%2"
+cmake -H. -B_project -A "x64" -DCMAKE_INSTALL_PREFIX=%@location% 
 cmake --build _project --config Release --target INSTALL
 GOTO DONE
 
 :CLEAN
 rmdir /Q /S _project
-rmdir /Q /S _bin
+rmdir /Q /S _install
 GOTO DONE
 
 :DONE
