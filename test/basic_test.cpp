a25cd0c2e9b34612a0ad06ff6ae891647b370cda
PreyMa <mlpreymann@gmail.com>
2023-04-07 10:57:27 +0200

    Use exceptions for error handling

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -25,65 +25,100 @@ void test1()
 	ccli::Var<bool> boolVar3("b3", "bool3", 1, ccli::NONE, "Third bool Var");
 	ccli::Var<bool> boolVar4("b4", "bool4", true, ccli::NONE, "Fourth bool Var");
 
-	assert(boolVar1.getValue() == false);
-	assert(boolVar2.getValue() == false);
-	assert(boolVar3.getValue() == true);
-	assert(boolVar4.getValue() == true);
-	for (const std::string& s : ccli::checkErrors()) std::cout << "\t\t" << s << std::endl << std::endl;
+	try {
+		assert(boolVar1.getValue() == false);
+		assert(boolVar2.getValue() == false);
+		assert(boolVar3.getValue() == true);
+		assert(boolVar4.getValue() == true);
+	}
+	catch (ccli::CCLIError& e) {
+		std::cout << "Caught error: " << e.message() << std::endl;
+	}
 
-	const char* argv[] = { "-b1", "1", "-b2", "true", "-b3", "0", "-b4", "false" };
-	ccli::parseArgs(std::size(argv), argv);
-	assert(boolVar1.getValue() == true);
-	assert(boolVar2.getValue() == true);
-	assert(boolVar3.getValue() == false);
-	assert(boolVar4.getValue() == false);
-	for (const std::string& s : ccli::checkErrors()) std::cout << "\t\t" << s << std::endl << std::endl;
+	try{
+		const char* argv[] = { "-b1", "1", "-b2", "true", "-b3", "0", "-b4", "false" };
+		ccli::parseArgs(std::size(argv), argv);
+		assert(boolVar1.getValue() == true);
+		assert(boolVar2.getValue() == true);
+		assert(boolVar3.getValue() == false);
+		assert(boolVar4.getValue() == false);
+	}
+	catch (ccli::CCLIError& e) {
+		std::cout << "Caught error: " << e.message() << std::endl;
+	}
 
-	const char* argv2[] = { "-b1", "0", "-b2", "-b3", "--bool4" };
-	ccli::parseArgs(std::size(argv2), argv2);
-	assert(boolVar1.getValue() == false);
-	assert(boolVar2.getValue() == true);
-	assert(boolVar3.getValue() == true);
-	assert(boolVar4.getValue() == true);
-	for (const std::string& s : ccli::checkErrors()) std::cout << "\t\t" << s << std::endl;
+	try {
+		const char* argv2[] = { "-b1", "0", "-b2", "-b3", "--bool4" };
+		ccli::parseArgs(std::size(argv2), argv2);
+		assert(boolVar1.getValue() == false);
+		assert(boolVar2.getValue() == true);
+		assert(boolVar3.getValue() == true);
+		assert(boolVar4.getValue() == true);
+	}
+	catch (ccli::CCLIError& e) {
+		std::cout << "Caught error: " << e.message() << std::endl;
+	}
 }
 
 void test2()
 {
-	ccli::Var<uint32_t, 3> uvec3Var("uvec3", "", {1, 2, 3});
-	ccli::Var<std::string, 2> stringVar("string", "", { "This is a test", "really"});
+	ccli::Var<uint32_t, 3> uvec3Var("uvec3", "", { 1, 2, 3 });
+	ccli::Var<std::string, 2> stringVar("string", "", { "This is a test", "really" });
 	ccli::Var<int8_t, 2, ccli::MaxLimit<2>> limitVar("limit", "", { 3, 4 });
 
-	assert(uvec3Var.getValue().at(0) == 1 && uvec3Var.getValue().at(1) == 2 && uvec3Var.getValue().at(2) == 3);
-	assert(stringVar.getValue().at(0) == "This is a test" && stringVar.getValue().at(1) == "really");
-	assert(limitVar.getValue().at(0) == 2 && limitVar.getValue().at(1) == 2);
-	for (const std::string& s : ccli::checkErrors()) std::cout << "\t\t" << s << std::endl;
+	try {
+		assert(uvec3Var.getValue().at(0) == 1 && uvec3Var.getValue().at(1) == 2 && uvec3Var.getValue().at(2) == 3);
+		assert(stringVar.getValue().at(0) == "This is a test" && stringVar.getValue().at(1) == "really");
+		assert(limitVar.getValue().at(0) == 2 && limitVar.getValue().at(1) == 2);
+	}
+	catch (ccli::CCLIError& e) {
+		std::cout << "Caught error: " << e.message() << std::endl;
+	}
 
-	const char* argv[] = { "-uvec3", "5,6,7", "-string", "This is not a test,or is it", "-limit", "100,1"};
-	ccli::parseArgs(std::size(argv), argv);
-	assert(uvec3Var.getValue().at(0) == 5 && uvec3Var.getValue().at(1) == 6 && uvec3Var.getValue().at(2) == 7);
-	assert(stringVar.getValue().at(0) == "This is not a test" && stringVar.getValue().at(1) == "or is it");
-	assert(limitVar.getValue().at(0) == 2 && limitVar.getValue().at(1) == 1);
-	for (const std::string& s : ccli::checkErrors()) std::cout << "\t\t" << s << std::endl;
+	try {
+		const char* argv[] = { "-uvec3", "5,6,7", "-string", "This is not a test,or is it"};
+		ccli::parseArgs(std::size(argv), argv);
+		assert(uvec3Var.getValue().at(0) == 5 && uvec3Var.getValue().at(1) == 6 && uvec3Var.getValue().at(2) == 7);
+		assert(stringVar.getValue().at(0) == "This is not a test" && stringVar.getValue().at(1) == "or is it");
+		assert(limitVar.getValue().at(0) == 2 && limitVar.getValue().at(1) == 1);
+	}
+	catch (ccli::CCLIError& e) {
+		std::cout << "Caught error: " << e.message() << std::endl;
+	}
+}
+
+void test3() {
+	try {
+		throw ccli::FileError{ "a/file/name" };
+	}
+	catch (const std::exception& e) {
+		std::cout << "Caught error: " << e.what() << std::endl;
+	}
 }
 
 int main(int argc, char* argv[]) {
 	test1();
 	test2();
+	test3();
 
 	ccli::Var<float, 4, ccli::MaxLimit<1.0>, ccli::MinLimit<-1>> float_var1("f1", "float1", {0}, ccli::NONE, "First bool Var");
 	ccli::Var<float, 4> float_var2("f2", "float2", {0}, ccli::NONE, "First bool Var");
 	ccli::Var<float, 2> var_test("t", "test", { 100, 200 }, ccli::CONFIG_RDWR);
 	ccli::Var<short, 1, ccli::MaxLimit<500>> short_var("s", "short", 0);
 	ccli::Var<bool> bool_var("b", "bool1", false);
-	ccli::parseArgs(argc, argv);
-	ccli::loadConfig("test.cfg");
-	var_test.setValue({ 200, 200 });
-	ccli::writeConfig("test.cfg");
-	std::cout << float_var1.getValue()[0] << std::endl;
-	std::cout << float_var1.getValue()[1] << std::endl;
-	std::cout << var_test.getValue()[0] << std::endl;
-	for (const std::string& s : ccli::checkErrors()) std::cout << s << std::endl;
+
+	try {
+		ccli::parseArgs(argc, argv);
+		ccli::loadConfig("test.cfg");
+		var_test.setValue({ 200, 200 });
+		ccli::writeConfig("test.cfg");
+		std::cout << float_var1.getValue()[0] << std::endl;
+		std::cout << float_var1.getValue()[1] << std::endl;
+		std::cout << var_test.getValue()[0] << std::endl;
+	}
+	catch (ccli::CCLIError& e) {
+		std::cout << "Caught error: " << e.message() << std::endl;
+	}
 
 	std::cout << "Sizeof float_var1: " << sizeof(float_var1) << std::endl;
 	std::cout << "Sizeof float_var2: " << sizeof(float_var2) << std::endl;
