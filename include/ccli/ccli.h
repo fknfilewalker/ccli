aaf9e833e8184d3a0bf7eaa1fab63a62d9e520ab
Lukas Lipp <llipp@cg.tuwien.ac.at>
2023-04-11 14:00:14 +0200

    license

--- include/ccli/ccli.h
+++ include/ccli/ccli.h
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright(c) 2022 Lukas Lipp, Matthias Preymann
+Copyright(c) 2023 Lukas Lipp, Matthias Preymann
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this softwareand associated documentation files(the "Software"), to deal
